5ae444d7a4c7c232374c2f7460a3e3c04d7cfb38
SeanChang-3245 <70448313+SeanChang-3245@users.noreply.github.com>
2024-11-07 21:05:36 +0800

    update translate_folder.py

--- bird_name_translation/translate_folder.py
+++ bird_name_translation/translate_folder.py
@@ -10,14 +10,16 @@ def translate_folder_names(base_folder, translations):
     for folder_name in os.listdir(base_folder):
         folder_path = os.path.join(base_folder, folder_name)
         if os.path.isdir(folder_path):
-            english_name = translations.get(folder_name)
+            base_name = folder_name.replace('_5secs', '')
+            english_name = translations.get(base_name)
             if english_name:
-                new_folder_path = os.path.join(base_folder, english_name)
+                new_folder_name = english_name 
+                new_folder_path = os.path.join(base_folder, new_folder_name)
                 os.rename(folder_path, new_folder_path)
                 print(f'Renamed: {folder_path} -> {new_folder_path}')
 
 if __name__ == "__main__":
-    base_folder = './dataset'
+    base_folder = 'F:\\Sean\\classified_data'
     json_file = './bird_name_translation/chinese_to_english.json'
     
     translations = load_translations(json_file)
